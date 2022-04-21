3b822f5dc4b2265e10a46ba48f5e46ccebf0840b
valentina.skakun <valentina.skakun@inn.ru>
2022-04-21 23:51:41 +0300

    metricsServer 0.1.0

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"math/rand"
 	"net/http"
 	"os"
@@ -32,33 +31,62 @@ var reportInterval time.Duration = 10000 //Milliseconds
 var serverToSendAddress = "127.0.0.1:8080"
 var serverToSendProto = "http"
 
+//добавить возможность вытаскивать конфиг только по нужным метрикам (?)
+//var configMetrics = []string{"alloc", "buckHashSys"}
+
 func getCurrentRuntimeMemStats() (currentMemStats runtime.MemStats) {
 	runtime.ReadMemStats(&currentMemStats)
 	return currentMemStats
 }
-func updateValuesRuntime(currentMemStats runtime.MemStats, metricsForUpdate *Metrics) {
-	metricsForUpdate.gaugeMetric["alloc"] = float64(currentMemStats.Alloc)
-	metricsForUpdate.gaugeMetric["buckHashSys"] = float64(currentMemStats.BuckHashSys)
+func updateValuesRuntime(currentMemStats runtime.MemStats, metricsForUpdate Metrics) {
+	metricsForUpdate.gaugeMetric["Alloc"] = float64(currentMemStats.Alloc)
+	metricsForUpdate.gaugeMetric["BuckHashSys"] = float64(currentMemStats.BuckHashSys)
+	metricsForUpdate.gaugeMetric["Frees"] = float64(currentMemStats.Frees)
+	metricsForUpdate.gaugeMetric["GCCPUFraction"] = float64(currentMemStats.GCCPUFraction)
+	metricsForUpdate.gaugeMetric["GCSys"] = float64(currentMemStats.GCSys)
+	metricsForUpdate.gaugeMetric["HeapAlloc"] = float64(currentMemStats.HeapAlloc)
+	metricsForUpdate.gaugeMetric["HeapIdle"] = float64(currentMemStats.HeapIdle)
+	metricsForUpdate.gaugeMetric["HeapInuse"] = float64(currentMemStats.HeapInuse)
+	metricsForUpdate.gaugeMetric["HeapObjects"] = float64(currentMemStats.HeapObjects)
+	metricsForUpdate.gaugeMetric["HeapReleased"] = float64(currentMemStats.HeapReleased)
+	metricsForUpdate.gaugeMetric["HeapSys"] = float64(currentMemStats.HeapSys)
+	metricsForUpdate.gaugeMetric["LastGC"] = float64(currentMemStats.LastGC)
+	metricsForUpdate.gaugeMetric["Lookups"] = float64(currentMemStats.Lookups)
+	metricsForUpdate.gaugeMetric["MCacheInuse"] = float64(currentMemStats.MCacheInuse)
+	metricsForUpdate.gaugeMetric["MCacheSys"] = float64(currentMemStats.MCacheSys)
+	metricsForUpdate.gaugeMetric["MSpanInuse"] = float64(currentMemStats.MSpanInuse)
+	metricsForUpdate.gaugeMetric["MSpanSys"] = float64(currentMemStats.MSpanSys)
+	metricsForUpdate.gaugeMetric["Mallocs"] = float64(currentMemStats.Mallocs)
+	metricsForUpdate.gaugeMetric["NextGC"] = float64(currentMemStats.NextGC)
+	metricsForUpdate.gaugeMetric["NumForcedGC"] = float64(currentMemStats.NumForcedGC)
+	metricsForUpdate.gaugeMetric["NumGC"] = float64(currentMemStats.NumGC)
+	metricsForUpdate.gaugeMetric["OtherSys"] = float64(currentMemStats.OtherSys)
+	metricsForUpdate.gaugeMetric["PauseTotalNs"] = float64(currentMemStats.PauseTotalNs)
+	metricsForUpdate.gaugeMetric["StackInuse"] = float64(currentMemStats.StackInuse)
+	metricsForUpdate.gaugeMetric["StackSys"] = float64(currentMemStats.StackSys)
+	metricsForUpdate.gaugeMetric["Sys"] = float64(currentMemStats.Sys)
+	metricsForUpdate.gaugeMetric["TotalAlloc"] = float64(currentMemStats.TotalAlloc)
 }
-func updateMetrics(metricsToUpdate *Metrics) {
+func updateMetrics(metricsToUpdate Metrics) {
 	updateValuesRuntime(getCurrentRuntimeMemStats(), metricsToUpdate)
 	metricsToUpdate.counterMetric["pollCount"] += 1
 	metricsToUpdate.gaugeMetric["randomValue"] = rand.Float64()
 	metricsToUpdate.timeMetric = time.Now()
 	fmt.Println(time.Now(), "updating")
 }
-func sendMetrics(metricsToSend *Metrics) {
+func sendMetrics(metricsToSend Metrics) {
 	for key, value := range metricsToSend.gaugeMetric {
 		//fmt.Println("sendMetrics gauge", key, value)
 		sendPOST("update", "gauge", key, fmt.Sprintf("%f", value))
+		//fmt.Println(test.StatusCode)
 	}
 	for key, value := range metricsToSend.counterMetric {
 		//fmt.Println("sendMetrics counter", key, value)
 		sendPOST("update", "counter", key, strconv.FormatInt(value, 10))
 	}
-	fmt.Println("counter", metricsToSend.counterMetric["pollCount"])
+	//fmt.Println("counter", metricsToSend.counterMetric["pollCount"])
 }
-func sendPOST(urlAction string, urlMetricType string, urlMetricKey string, urlMetricValue string) {
+func sendPOST(urlAction string, urlMetricType string, urlMetricKey string, urlMetricValue string) *http.Response {
 	//http://<АДРЕС_СЕРВЕРА>/update/<ТИП_МЕТРИКИ>/<ИМЯ_МЕТРИКИ>/<ЗНАЧЕНИЕ_МЕТРИКИ>
 	url := serverToSendProto + "://" + serverToSendAddress + "/" + urlAction + "/"
 	url += urlMetricType + "/" + urlMetricKey + "/" + urlMetricValue
@@ -66,23 +94,23 @@ func sendPOST(urlAction string, urlMetricType string, urlMetricKey string, urlMe
 	//contentType := "Content-Type: text/plain"
 	client := &http.Client{}
 	req, err := http.NewRequest(method, url, nil)
+	resEmpty := new(http.Response)
 	req.Header.Add("Content-Type", "Content-Type: text/plain")
 	if err != nil {
 		fmt.Println(err)
-		return
+		return resEmpty
 	}
 	res, err := client.Do(req)
 	if err != nil {
 		fmt.Println(err)
-		return
+		return res
 	}
 	defer res.Body.Close()
-	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		fmt.Println(err)
-		return
+		return res
 	}
-	fmt.Println(string(body))
+	return res
 }
 func inBackgroundMetrics(tickerToBackground *time.Ticker, metricsToBackground *Metrics, functionToBackground func(*Metrics)) {
 	for range tickerToBackground.C {
@@ -95,11 +123,9 @@ func handleSignal(signal os.Signal) {
 	os.Exit(-1)
 }
 func main() {
-	//currentMemStats := getCurrentRuntimeMemStats()
-
 	tickerPoll := time.NewTicker(time.Millisecond * pollInterval)
 	tickerReport := time.NewTicker(time.Millisecond * reportInterval)
-	metricsAgent := new(Metrics)
+	var metricsAgent Metrics
 	metricsAgent.gaugeMetric = make(map[string]float64)
 	metricsAgent.counterMetric = make(map[string]int64)
 	sigs := make(chan os.Signal, 1)
@@ -121,4 +147,5 @@ func main() {
 		}
 	}()
 	select {}
+
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,63 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type Metrics struct {
+	gaugeMetric   map[string]float64
+	counterMetric map[string]int64
+	timeMetric    time.Time
+}
+
+var serverToGetAddress = "127.0.0.1:8080"
+
+var MetricsRun Metrics
+
+//var serverToGetProto = "http"
+func listMetrics(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "METRICS:")
+	for key, value := range MetricsRun.gaugeMetric {
+		fmt.Fprintln(w, key, value)
+	}
+	for key, value := range MetricsRun.counterMetric {
+		fmt.Fprintln(w, key, value)
+	}
+
+}
+func updateMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(r.RequestURI + "\n"))
+	splitUrl := strings.Split(r.RequestURI, "/")
+	if len(splitUrl) == 5 {
+		if splitUrl[2] == "gauge" {
+			metricName := splitUrl[3]
+			metricValue, _ := strconv.ParseFloat(splitUrl[4], 64)
+			MetricsRun.gaugeMetric[metricName] = metricValue
+		}
+		if splitUrl[2] == "counter" {
+			metricName := splitUrl[3]
+			metricValue, _ := strconv.ParseInt(splitUrl[4], 10, 64)
+			MetricsRun.counterMetric[metricName] += metricValue
+		}
+	}
+}
+
+func main() {
+
+	MetricsRun.gaugeMetric = make(map[string]float64)
+	MetricsRun.counterMetric = make(map[string]int64)
+
+	// маршрутизация запросов обработчику
+	http.HandleFunc("/", listMetrics)
+	http.HandleFunc("/update/", updateMetrics)
+	// конструируем свой сервер
+	server := &http.Server{
+		Addr: serverToGetAddress,
+	}
+
+	server.ListenAndServe()
+}
