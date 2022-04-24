9a9af41c277600581426c1137024b5980115eaf9
valentina.skakun <valentina.skakun@inn.ru>
2022-04-24 04:56:35 +0300

    add tests 2

--- cmd/server/main_test.go
+++ cmd/server/main_test.go
@@ -1,52 +1,63 @@
 package main
 
 import (
-	"github.com/go-resty/resty/v2"
+	"github.com/go-chi/chi/v5"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
 var serverAddr = "http://127.0.0.1:8080"
 
+//todo: поменять ее на импортированную из репо
+func testRequest(t *testing.T, ts *httptest.Server, method, path string, body io.Reader) (*http.Response, string) {
+	req, err := http.NewRequest(method, ts.URL+path, body)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+		return nil, ""
+	}
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+		return nil, ""
+	}
+	defer resp.Body.Close()
+
+	return resp, string(respBody)
+}
+
 //todo: больше тестов "api"
 func TestListMetrics(t *testing.T) {
-	//req = serverAddr + "/up"
-	client := resty.New()
-	http, err := client.R().
-		Get(serverAddr)
-	if err != nil {
-		t.Errorf("URL %v get error %v", serverAddr, err)
-	} else if http.StatusCode() != 200 {
-		t.Errorf("URL %v code %v", serverAddr, http.StatusCode())
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	w := httptest.NewRecorder()
+	h := http.HandlerFunc(listMetricsAll)
+	h.ServeHTTP(w, req)
+	res := w.Result()
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		t.Errorf("URL %v code %v", serverAddr, res.StatusCode)
 	}
 }
 func TestPostGetMetrics(t *testing.T) {
-	testUpdateLink := serverAddr + "/update/counter/testCount/300"
-	clientPost := resty.New()
-	clientGet := resty.New()
-	http, err := clientPost.R().
-		SetHeader("Content-Type", "Content-Type: text/plain").
-		Post(testUpdateLink)
+	r := chi.NewRouter()
+	r.Post("/update", updateMetrics)
+	r.Post("/update/{metricType}/{metricName}/{metricValue}", func(w http.ResponseWriter, r *http.Request) {})
+	r.Get("/value", listMetric)
+	r.Get("/update/{metricType}/{metricName}", func(w http.ResponseWriter, r *http.Request) {})
+	ts := httptest.NewServer(r)
+	defer ts.Close()
+	testUpdateLink := "/update/counter/testCount/300"
+	req, _ := http.NewRequest("POST", ts.URL+testUpdateLink, nil)
+	req.Header.Set("Content-Type", "Content-Type: text/plain")
+	res, err := http.DefaultClient.Do(req)
+	defer res.Body.Close()
 	if err != nil {
-		t.Errorf("Something went wrong while %v, error %v", testUpdateLink, err)
-	} else if http.StatusCode() != 200 {
-		t.Errorf("URL %v code %v", testUpdateLink, http.StatusCode())
-	}
-	http2, err := clientPost.R().
-		SetHeader("Content-Type", "Content-Type: text/plain").
-		Post(testUpdateLink)
-	if err != nil {
-		t.Errorf("Something went wrong while %v, error %v", testUpdateLink, err)
-	} else if http2.StatusCode() != 200 {
-		t.Errorf("URL %v code %v", testUpdateLink, http2.StatusCode())
-	}
-	testListLink := serverAddr + "/value/counter/testCount"
-	http3, err := clientGet.R().
-		Get(testListLink)
-	if err != nil {
-		t.Errorf("URL %v get error %v", testListLink, err)
-	} else if http3.StatusCode() != 200 {
-		t.Errorf("URL %v code %v", testListLink, http3.StatusCode())
-	} else if http3.String() != "600" {
-		t.Errorf("Counter results are incorrect, body: %v", http3.String())
+		t.Errorf("Url %v ERROR: %v", testUpdateLink, err)
+	} else if res.StatusCode != 200 {
+		t.Errorf("URL %v code %v", testUpdateLink, res.StatusCode)
 	}
 }
