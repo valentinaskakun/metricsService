ee0ddb5f6a11fe0d3b3eedc19afcb7fec6e9e61d
valentina.skakun <valentina.skakun@inn.ru>
2022-05-16 02:54:09 +0300

    increment 4 0.1

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -23,6 +23,13 @@ type Metrics struct {
 	timeMetric    time.Time
 }
 
+type MetricsJSON struct {
+	ID    string   `json:"id"`              // имя метрики
+	MType string   `json:"type"`            // параметр, принимающий значение gauge или counter
+	Delta *int64   `json:"delta,omitempty"` // значение метрики в случае передачи counter
+	Value *float64 `json:"value,omitempty"` // значение метрики в случае передачи gauge
+}
+
 const (
 	pollIntervalConst   = 2000
 	reportIntervalConst = 10000
@@ -102,6 +109,31 @@ func sendPOST(urlString string) {
 		return
 	}
 }
+
+//todo: переделать использование url server path
+func sendMetricJSON(metricsToSend *Metrics, serverToSendLink string) {
+	if metricsToSend.counterMetric["pollCount"] != 0 {
+		urlStr, _ := url.Parse(serverToSendLink)
+		urlStr.Path = path.Join(urlStr.Path, "update")
+		client := resty.New()
+		client.R().
+			SetHeader("Content-Type", "Content-Type: application/json")
+		for key, value := range metricsToSend.gaugeMetric {
+			metricToSend := MetricsJSON{ID: key, MType: "gauge", Value: &value}
+			client.R().
+				SetBody(metricToSend).
+				Post(urlStr.Path)
+		}
+		for key, value := range metricsToSend.counterMetric {
+			metricToSend := MetricsJSON{ID: key, MType: "counter", Delta: &value}
+			client.R().
+				SetBody(metricToSend).
+				Post(urlStr.Path)
+		}
+	} else {
+		fmt.Println("ERROR: " + "pollCounter is 0")
+	}
+}
 func handleSignal(signal os.Signal) {
 	fmt.Println("* Got:", signal)
 	os.Exit(-1)
@@ -131,12 +163,10 @@ func main() {
 	}()
 	go func() {
 		for range tickerReport.C {
-			sendMetrics(&MetricsCurrent, serverToSend)
-			MetricsCurrent.muGauge.Lock()
+			sendMetricJSON(&MetricsCurrent, serverToSend)
 			MetricsCurrent.muCounter.Lock()
 			MetricsCurrent.counterMetric = updateCounterMetrics("init", MetricsCurrent.counterMetric)
 			MetricsCurrent.muCounter.Unlock()
-			MetricsCurrent.muGauge.Unlock()
 		}
 	}()
 	select {}
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/go-chi/chi/v5"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"strconv"
@@ -10,13 +12,21 @@ import (
 	"time"
 )
 
-//вынести структуру в модуль (?)
+//todo: вынести структуру в модуль + реализовать интерфейс хранения
+
 type Metrics struct {
 	muGauge       sync.RWMutex
 	gaugeMetric   map[string]float64
 	muCounter     sync.RWMutex
 	counterMetric map[string]int64
 	timeMetric    time.Time
+}
+
+type MetricsJSON struct {
+	ID    string   `json:"id"`              // имя метрики
+	MType string   `json:"type"`            // параметр, принимающий значение gauge или counter
+	Delta *int64   `json:"delta,omitempty"` // значение метрики в случае передачи counter
+	Value *float64 `json:"value,omitempty"` // значение метрики в случае передачи gauge
 }
 
 var serverToGetAddress = "127.0.0.1:8080"
@@ -26,6 +36,7 @@ var MetricsRun Metrics
 //todo: вынести хэндлеры в интернал
 func listMetricsAll(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "METRICS GAUGE:")
+	//todo: нужно ли добавлять RLock
 	for key, value := range MetricsRun.gaugeMetric {
 		fmt.Fprintln(w, key, value)
 	}
@@ -54,7 +65,41 @@ func listMetric(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-//todo: разнести по типам (?)
+func listMetricJSON(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	metricReq := MetricsJSON{}
+	metricRes := MetricsJSON{}
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+	if err := json.Unmarshal(body, &metricReq); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+	if metricReq.MType == "gauge" {
+		if _, ok := MetricsRun.gaugeMetric[metricReq.ID]; ok {
+			metricRes.ID, metricRes.MType = metricReq.ID, metricReq.MType
+			valueRes := MetricsRun.gaugeMetric[metricReq.ID]
+			metricRes.Value = &valueRes
+		}
+	} else if metricReq.MType == "counter" {
+		if _, ok := MetricsRun.counterMetric[metricReq.ID]; ok {
+			metricRes.ID, metricRes.MType = metricReq.ID, metricReq.MType
+			valueRes := MetricsRun.counterMetric[metricReq.ID]
+			metricRes.Delta = &valueRes
+		}
+	} else {
+		w.WriteHeader(http.StatusNotImplemented)
+	}
+	if resBody, err := json.Marshal(metricRes); err != nil {
+		http.Error(w, err.Error(), http.StatusUnprocessableEntity)
+		return
+	} else {
+		w.WriteHeader(http.StatusOK)
+		w.Write(resBody)
+	}
+}
+
 func updateMetrics(w http.ResponseWriter, r *http.Request) {
 	metricType := chi.URLParam(r, "metricType")
 	metricName := chi.URLParam(r, "metricName")
@@ -62,7 +107,7 @@ func updateMetrics(w http.ResponseWriter, r *http.Request) {
 	if metricType == "gauge" {
 		valParsed, err := strconv.ParseFloat(metricValue, 64)
 		if err != nil {
-			w.WriteHeader(400)
+			w.WriteHeader(http.StatusBadRequest)
 		} else {
 			MetricsRun.muGauge.Lock()
 			MetricsRun.gaugeMetric[metricName] = valParsed
@@ -71,15 +116,45 @@ func updateMetrics(w http.ResponseWriter, r *http.Request) {
 	} else if metricType == "counter" {
 		valParsed, err := strconv.ParseInt(metricValue, 10, 64)
 		if err != nil {
-			w.WriteHeader(400)
+			w.WriteHeader(http.StatusBadRequest)
 		} else {
 			MetricsRun.muCounter.Lock()
 			MetricsRun.counterMetric[metricName] += valParsed
 			MetricsRun.muCounter.Unlock()
 		}
 	} else {
-		w.WriteHeader(501)
+		w.WriteHeader(http.StatusNotImplemented)
 	}
+}
+
+func updateMetricJSON(w http.ResponseWriter, r *http.Request) {
+	metricReq := MetricsJSON{}
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+	if err := json.Unmarshal(body, &metricReq); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+	if metricReq.MType == "gauge" {
+		MetricsRun.muGauge.Lock()
+		MetricsRun.gaugeMetric[metricReq.ID] = *metricReq.Value
+		MetricsRun.muGauge.Unlock()
+	} else if metricReq.MType == "counter" {
+		MetricsRun.muCounter.Lock()
+		MetricsRun.counterMetric[metricReq.ID] += *metricReq.Delta
+		MetricsRun.muCounter.Unlock()
+	} else {
+		w.WriteHeader(http.StatusNotImplemented)
+	}
+
+	//if response, err := json.Marshal(); err != nil {
+	//	http.Error(w, err.Error(), http.StatusUnprocessableEntity)
+	//	return
+	//} else {
+	//	w.WriteHeader(http.StatusOK)
+	//	w.Write(response)
+	//}
 }
 
 func main() {
@@ -91,11 +166,13 @@ func main() {
 		r.Route("/{metricType}", func(r chi.Router) {
 			r.Post("/{metricName}/{metricValue}", updateMetrics)
 		})
+		r.Post("/", updateMetricJSON)
 	})
 	r.Route("/value", func(r chi.Router) {
 		r.Route("/{metricType}", func(r chi.Router) {
-			r.Get("/{metricName}", listMetric)
+			r.Post("/{metricName}", listMetric)
 		})
+		r.Post("/", listMetricJSON)
 	})
 	log.Fatal(http.ListenAndServe(serverToGetAddress, r))
 }
