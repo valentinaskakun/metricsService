1811fb4a49a7a594985b254a27c524a1cc9e0a79
valentina.skakun <valentina.skakun@inn.ru>
2022-06-21 02:33:54 +0300

    incr8 test

--- internal/compress/compress.go
+++ internal/compress/compress.go
@@ -18,7 +18,15 @@ func (w gzipWriter) Write(b []byte) (int, error) {
 }
 func GzipHandle(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		//if r.Header.Get("Content-Encoding"),
+		if r.Header.Get(`Content-Encoding`) == `gzip` {
+			gz, err := gzip.NewReader(r.Body)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			defer gz.Close()
+			r.Body = gz
+		}
 		// проверяем, что клиент поддерживает gzip-сжатие
 		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
 			// если gzip не поддерживается, передаём управление
